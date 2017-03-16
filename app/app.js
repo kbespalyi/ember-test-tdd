7162d84a4c00a1d15df52d486c8977cee112161f
Khusein Bespalyi <khusein.bespalyi@otolane.com>
2017-03-15 22:44:51 -0400

    added the user model test unit

--- app/app.js
+++ app/app.js
@@ -13,6 +13,12 @@ App = Ember.Application.extend({
   Resolver
 });
 
+App.ApplicationRoute = Ember.Route.extend({
+  model: function() {
+    return Ember.$.getJSON('https://api.github.com/repos/emberjs/ember.js/pulls?page=1&per_page=3');
+  }
+});
+
 loadInitializers(App, config.modulePrefix);
 
 export default App;
--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'ember-quickstart',
+    modulePrefix: 'ember-test-tdd',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
